eec2bdba3df2f08eb04707c98a2c15482f1d1a11
cam.lafit <cam.lafit@azerttyu.net>
2017-01-23 15:19:01 +0100

    A script to create an borg backup by each alternc user
    * This script do a backup by alternc user
    * A borg repository is set by user if it's required
    
    Note :
    * No control data
    * No prune
    * /var/backups/borg is required before to run this script

---
+++ src/usr/lib/alternc/borgbackup.sh
@@ -0,0 +1,36 @@
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+umask 022
+
+CONFIG_FILE="/usr/lib/alternc/functions.sh"
+export BORG_PASSPHRASE='PASSPHRASSE'
+
+
+
+if [ ! -r "$CONFIG_FILE" ]; then
+    echo "Can't access $CONFIG_FILE."
+    exit 1
+fi
+source "$CONFIG_FILE"
+
+if [ `id -u` -ne 0 ]; then
+    echo "$0 must be launched as root"
+    exit 1
+fi
+
+borg_execution() {
+        read GID LOGIN || true
+        while [ "$LOGIN" ] ; do
+                REP="$(get_html_path_by_name "$LOGIN")"
+                REPO="/var/backups/borg/$LOGIN"
+                ARCHIVE_NAME=$(date +%Y%M%d)
+
+                if [ ! -d $REPO ]; then
+                        borg init $REPO
+                fi
+                borg create -v --stats $REPO::$ARCHIVE_NAME $REP
+
+        read GID LOGIN || true
+        done
+}
+
+mysql --defaults-file=/etc/alternc/my.cnf --skip-column-names -B -e "SELECT uid,login FROM membres ORDER BY login" | borg_execution
