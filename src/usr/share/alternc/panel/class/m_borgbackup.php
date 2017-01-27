1eafcdef8ee5dce4e3b67da4f5dfca7ecd50ac24
cam.lafit <cam.lafit@azerttyu.net>
2017-01-27 15:08:19 +0100

    List all backup available from current user
    qlist() return
    * false on any error
    * an array, each row with a named string and datetime creation

--- src/usr/share/alternc/panel/class/m_borgbackup.php
+++ src/usr/share/alternc/panel/class/m_borgbackup.php
@@ -2,6 +2,10 @@
 
 class m_borgbackup {
 
+    private $borgbackup_bin="";
+    private $borgbackup_passphrase="";
+    private $backup_path="";
+
     function hook_menu() {
 
         $obj = array(
@@ -22,4 +26,26 @@ class m_borgbackup {
         return $obj;
     }
 
+    function qlist() {
+
+        global $mem;
+
+        $result = false;
+        $exec = "export BORG_PASSPHRASE='".$this->borgbackup_passphrase."'; ".$this->borgbackup_bin." list ".$this->backup_path."/".$mem->user['login'];
+
+        if (exec($exec,$output,$return_var)) {
+            $result = array();
+            foreach($output as $row) {
+                  $cells=array();
+                  preg_match('/^(\w*)\s+([\w\s,-:]*)$/',$row,$cells);
+                  $result[] = array(
+                       'name' => $cells[1],
+                       'date' => new DateTime($cells[2])
+                  );
+            }
+        }
+
+        return $result;
+    }
 }
+
