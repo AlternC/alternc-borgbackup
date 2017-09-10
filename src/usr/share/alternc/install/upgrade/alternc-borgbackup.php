866ece5058541c9a1e7f5447cdc9124183549fef
cam.lafit <cam.lafit@azerttyu.net>
2017-09-10 11:54:25 +0200

    In any case we use a standard borg bin
    * In any case borgbackup is installed in /usr/bin

--- src/usr/share/alternc/install/upgrade/alternc-borgbackup.php
+++ src/usr/share/alternc/install/upgrade/alternc-borgbackup.php
@@ -12,7 +12,7 @@ if(!chdir("/usr/share/alternc/panel"))
 exit(1);
 require("/usr/share/alternc/panel/class/config_nochk.php");
 
-variable_set('borgbackup_bin','/usr/local/bin/borg', 'BorgBackup : absolute bin path (required with local wheezy install)');
+variable_set('borgbackup_bin','/usr/bin/borg', 'BorgBackup : absolute bin path (required with local wheezy install)');
 variable_set('borgbackup_passphrase',' ', 'BorgBackup  : Passphrase to protect archive, should be set');
 variable_set('borgbackup_backup_path','/var/backups/borg/', 'BorgBackup : Path where are stored all archive');
 variable_set('borgbackup_backup_dir_local','backup', 'BorgBackup : Local directory user account where archive are restored');
